d2f4c3936c91363fc5b6b200f6c8b735f32878b8
Erica Y Stevens <ericastevens99@gmail.com>
2016-09-25 20:20:48 -0400

    Homework Submission

--- Tableviews_Part_1/ViewControllers/MovieTableViewController.swift
+++ Tableviews_Part_1/ViewControllers/MovieTableViewController.swift
@@ -15,6 +15,17 @@ class MovieTableViewController: UITableViewController {
     internal let rawMovieData: [[String : Any]] = movies
     let cellIdentifier: String = "MovieTableViewCell"
     
+    enum Genre: Int {
+        case animation
+        case action
+        case drama
+    }
+    
+    enum Century: Int {
+        case twentiethCentury
+        case twentyFirstCentury
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -30,25 +41,111 @@ class MovieTableViewController: UITableViewController {
         movieData = movieContainer
     }
 
-
+    
+//    func byGenre(_ genre: Genre) -> [Movie]? {
+//    let filter: (Movie) -> Bool
+//    switch genre {
+//    case .action:
+//    filter = { (a) -> Bool in
+//    a.genre == "action"
+//    }
+//    case .animation:
+//    filter = { (a) -> Bool in
+//    a.genre == "animation"
+//    }
+//    case .drama:
+//    filter = { (a) -> Bool in
+//    a.genre == "drama"
+//    }
+//    
+//    }
+    func byCentury(_ year: Century) -> [Movie]? {
+        
+        let cFilter: (Movie) -> Bool
+        switch year {
+        case .twentiethCentury:
+            cFilter = { (a) -> Bool in
+                1901...2000 ~= a.year
+            }
+        case .twentyFirstCentury:
+            cFilter = { (a) -> Bool in
+                2001...2100 ~= a.year
+            }
+        }
+    
+    // after filtering, sort
+    let filtered = movieData?.filter(cFilter).sorted {  $0.year < $1.year }
+    
+    return filtered
+    }
+    
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
+        return 3
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return movieData?.count ?? 0
+//        guard let genre = Genre.init(rawValue: section), let data = byGenre(genre)
+//            else {
+//                return 0
+//        }
+//        
+//        return data.count
+        guard let century = Century.init(rawValue: section), let data = byCentury(century)
+            else {
+                return 0
+        }
+        
+        return data.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+//        
+//        guard let genre = Genre.init(rawValue: indexPath.section), let data = byGenre(genre)
+//            else {
+//                return cell
+//        }
+//        
+//        cell.textLabel?.text = data[indexPath.row].title
+//        cell.detailTextLabel?.text = String(data[indexPath.row].year)
+//        
+//        return cell
         let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
-        guard  let data = movieData else {
-            return cell
+        
+        
+        guard let century = Century.init(rawValue: indexPath.section), let data = byCentury(century)
+            else {
+                return cell
         }
-        cell.textLabel?.text = movieData?[indexPath.row].title
+        
+        cell.textLabel?.text = data[indexPath.row].title
         cell.detailTextLabel?.text = String(data[indexPath.row].year)
         
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+//        guard let genre = Genre.init(rawValue: section) else { return "" }
+//        
+//        switch genre {
+//        case .action:
+//            return "Action"
+//        case .animation:
+//            return "Animation"
+//        case .drama:
+//            return "Drama"
+//        }
+        
+        guard let century = Century.init(rawValue: section) else {
+            return "" }
+        
+        switch century {
+        case .twentiethCentury:
+            return "20th Century"
+        case .twentyFirstCentury:
+            return "21st Century"
+        }
     }
 }
